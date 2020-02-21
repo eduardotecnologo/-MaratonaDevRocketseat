6dab282ed7c336651d129896f210c0c18be90ad2
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-02-20 22:26:43 -0300

    setup nunjucks

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
+//Setup Server
 const express = require('express');
 const server = express();
+server.use(express.static('public'));
 
-server.listen(3000);
+//Setupe template engine nunjucks
+const nunjucks = require('nunjucks');
+nunjucks.configure('./', {
+  express: server
+});
+
+//Render Pages
+server.get('/', function(req, res) {
+  return res.render('index.html');
+});
+
+server.listen(3000, function() {
+  console.log('Start server');
+});
