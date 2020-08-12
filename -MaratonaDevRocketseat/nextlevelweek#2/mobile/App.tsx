81501804e46ecb57ecce400d2cf310e024360d42
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-08-11 23:09:31 -0300

    Create screens

--- -MaratonaDevRocketseat/nextlevelweek#2/mobile/App.tsx
+++ -MaratonaDevRocketseat/nextlevelweek#2/mobile/App.tsx
@@ -1,25 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import {Archivo_400Regular, Archivo_700Bold, useFonts} from '@expo-google-fonts/archivo';
+import {Poppins_400Regular, Poppins_600SemiBold} from '@expo-google-fonts/poppins';
+import AppStack from './src/routes/AppStack';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>App rodando no iPhone!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'green',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title:{
-    color:'#fff',
-    fontSize:32
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  });
+  if (!fontsLoaded){
+    return <AppLoading />;
+  }else{
+    return (
+      <>
+        <AppStack />
+        <StatusBar style="light" />
+      </>
+    );
   }
-});
+}
---
+++ -MaratonaDevRocketseat/nextlevelweek#2/mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View } from 'react-native';
+
+function GiveClass(){
+  return <View />
+}
+export default GiveClass;
