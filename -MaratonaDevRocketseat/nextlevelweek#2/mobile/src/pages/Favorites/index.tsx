2e6ca0fcff18f0dfbacdb85f1e6bbcb0c426f3c5
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-08-13 17:41:00 -0300

    Corrigir Buttons em Mestres Disponíveis

--- -MaratonaDevRocketseat/nextlevelweek#2/mobile/src/pages/Favorites/index.tsx
+++ -MaratonaDevRocketseat/nextlevelweek#2/mobile/src/pages/Favorites/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 import styles from './styles';
-import PageHeader from '../PageHeader';
+import PageHeader from '../../components/PageHeader';
 
 
 function Favorites() {
